7f929a86ab662fe79f2f5df2e5990874d28713e9
fsoc <8392143a@gmail.com>
2017-11-25 16:07:17 +0100

    Filter out folders from the image list.

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,8 @@ module.exports = router
 
 const fs = require('fs')
 
-router.get('/', (req, res) =>
-  res.render('index', { images: fs.readdirSync('./files') }))
+router.get('/', (req, res) => {
+  res.render('index', {
+    images: fs.readdirSync('./files').filter(e => e.indexOf('.') + 1)
+  })
+})
