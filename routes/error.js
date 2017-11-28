4277c7671c3069a4c85405195b7e6d1212b21133
fsoc <8392143a@gmail.com>
2017-11-28 13:31:05 +0100

    Log errors.

--- routes/error.js
+++ routes/error.js
@@ -1,8 +1,10 @@
 const handle = (err, req, res, next) => {
+  require('../logger').debug(err.message)
+
   res.status(500).render('status', {
     status: 'error',
-    head: err.message.length < 100 ? err.message : false,
-    message: err.message.length > 100 ? err.message : false
+    head: err.message.length < 100 ? err.message : '',
+    message: err.message.length > 100 ? err.message : ''
     // (err.message.length < 100 ? 'head' : 'message'): err.message // :(
   })
 
